2af84581a6ab215f1963d208ef813e5ff7e4e230
LegendaryEmp <jacobokwusi@gmail.com>
2023-04-30 21:16:27 +0100

    added week 2 project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage in number below: ");
+        int UserInput = int.Parse(Console.ReadLine());
+
+        if (UserInput >= 90)
+        {
+            Console.WriteLine("Congrats, you have made an A.");
+        }
+        else if (UserInput >= 80)
+        {
+            Console.WriteLine("Congrats, you have made a B.");
+        }
+        else if (UserInput >= 70)
+        {
+            Console.WriteLine("Congrats, you have made a C.");
+        }
+         else if (UserInput >= 60)
+        {
+            Console.WriteLine("Congrats, you have made a D.");
+        }
+         else if (UserInput < 60)
+        {
+            Console.WriteLine("Oops!!, you made F.");
+        }
     }
 }
