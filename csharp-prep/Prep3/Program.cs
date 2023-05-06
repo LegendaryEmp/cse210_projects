515611226fe77e5fb7183aea9d98dccadb0ba907
LegendaryEmp <jacobokwusi@gmail.com>
2023-05-06 22:04:30 +0100

    project work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Text;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public override string ToString()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        return $"{JobTitle} ({Company}) {StartYear}-{EndYear}";
+    }
+}
+
+public class Resume
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<Job> Jobs { get; set; }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(Name);
+        sb.AppendLine(Email);
+        sb.AppendLine(PhoneNumber);
+        foreach (Job job in Jobs)
+        {
+            sb.AppendLine(job.ToString());
+        }
+        return sb.ToString();
     }
 }
